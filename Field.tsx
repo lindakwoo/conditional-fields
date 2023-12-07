385b5a5923295bace95935a0b23742f57ad0fab5
Linda Woo <59804818+lindakwoo@users.noreply.github.com>
2023-12-07 03:46:14 -0800

    Update Field.tsx

--- Field.tsx
+++ Field.tsx
@@ -77,7 +77,7 @@ console.log('widget id', getWidgetId('gridCardsRow1', sdk) )
     } else {
       // this will eventually return null 
       // fix the css so that it works!!!
-      return <Paragraph className = "deleteMe" style = {{'&  .entry-edutir__field-group .deleteMe': {display:"none"}}} >nothing goes here!!!</Paragraph>
+      return <Paragraph className = "deleteMe" style = {{'&  .entry-editor__field-group .deleteMe': {display:"none"}}} >nothing goes here!!!</Paragraph>
     }
   } else {
     // TODO: default field here...
